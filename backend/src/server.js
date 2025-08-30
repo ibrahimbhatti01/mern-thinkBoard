12718b7ed2facb8b8561891b7aa61cb2ed3c754c
Ibrahim <ib.8686299@gmail.com>
2025-08-30 13:23:05 +0500

    some updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === "production"){
         app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")))
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(___dirname, "../frontend/vite-project", "dist", "index.jtlm"));
+        res.sendFile(path.join(__dirname, "../frontend/vite-project/dist/index.html"));
     })
 }
 
